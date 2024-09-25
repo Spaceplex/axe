894053351d2e418ccef30a9f239754b89bd8abdf
Spaceplex <spaecplex@gmail.com>
2024-09-25 01:06:37 -0400

    Initialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use axum::Router;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:3030").await.unwrap();
+
+    let service = Router::new();
+
+    axum::serve(listener, service.into_make_service())
+        .await
+        .unwrap();
 }
